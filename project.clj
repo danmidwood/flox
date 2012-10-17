b8575ab9e99ed54dc4cb1f9cfc7ad500ed3b6d01
Dan Midwood <dan@danmidwood.com>
2012-10-17 01:20:26 +0100

    Add midje support

--- project.clj
+++ project.clj
@@ -5,5 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/algo.generic "0.1.0"]
-                 [org.clojure/math.numeric-tower "0.0.1"]]
+                 [org.clojure/math.numeric-tower "0.0.1"]
+                 [midje "1.4.0"]]
+  :profiles {:dev {:plugins [[lein-midje "2.0.0-SNAPSHOT"]]}}
   :main flox.core)
