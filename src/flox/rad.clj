4be577b8088698b89d9e66db52edf69a869614b7
Dan Midwood <dan@danmidwood.com>
2012-11-23 22:15:41 +0000

    Begin implementation of rad, the new flox based on lazy seqs

---
+++ src/flox/rad.clj
@@ -0,0 +1,45 @@
+(ns flox.rad
+  (:require clojure.algo.generic.math-functions)
+  (:require clojure.math.numeric-tower))
+
+(import '(javax.sound.sampled AudioSystem DataLine$Info
+                              SourceDataLine
+                              AudioFormat AudioFormat$Encoding))
+
+(def line-data
+  {:sample-rate 48000
+   :audio-format (AudioFormat. 48000 16 2 true false)
+   :12th-root (clojure.math.numeric-tower/expt 2 (/ 1 12))})
+
+
+(defn frequency
+  "Calculate the frequency of a note offset from a base frequency."
+  ([] (frequency 0))
+  ([offset] (frequency offset 440.0) )
+  ([offset base]
+     (* base (clojure.math.numeric-tower/expt (:12th-root line-data) offset))))
+
+(defn find-sine
+  [distance]
+  (clojure.algo.generic.math-functions/sin (* 2 distance (. Math PI))))
+
+
+(defn freq-freq-lazy-seq 
+  "Create a lazy sequence of incrementing frequencies at one note per second with the specified sample-rate. If no sample-rate is supplied then 48000 is chosen"
+  ([base] (freq-freq-lazy-seq base 48000))
+  ([base sample-rate]
+     (lazy-seq
+      (cons base
+            (freq-freq-lazy-seq (frequency (/ 1 sample-rate) base) sample-rate)))))
+
+
+(defn get-note
+  ([offset] (get-note offset 1000))
+  ([offset ms]
+     (let
+         [freq (frequency offset)
+          period (/ (:sample-rate line-data) freq)]
+       (for [i (range (*  (:sample-rate line-data) (/ ms 1000)))]
+         (let [angle (/ (* 2 i (. Math PI)) period)]
+           (byte (* 127  (clojure.algo.generic.math-functions/sin angle))))))))
+
---
+++ test/flox/rad_test.clj
@@ -0,0 +1,71 @@
+(ns flox.rad-test
+  (:use clojure.test)
+  (:use flox.rad)
+  (:use midje.sweet)
+  (:use clojure.math.numeric-tower))
+
+
+
+(fact "audio format sample rate is 48000"
+      (int  (.getSampleRate (:audio-format line-data))) => 48000)
+
+(fact "audio format size is 16 bits"
+      (.getSampleSizeInBits (:audio-format line-data)) => 16)
+
+(fact "audio format is stereo"
+  (.getChannels (:audio-format line-data)) => 2)
+
+(fact "audio format frame size is four"
+      (.getFrameSize (:audio-format line-data)) => 4)
+
+(fact "audio format is little endian"
+      (not (.isBigEndian (:audio-format line-data))) => true)
+
+(fact "the twelth root is 1.059...."
+      (:12th-root line-data) => 1.0594630943592953)
+
+
+(defn nth-sig-fig
+  [n number]
+  (let
+      [scaler (clojure.math.numeric-tower/expt 10 n)]
+      (/ (round (* scaler number)) (float scaler))))
+
+(defn two-sig-fig
+  [x]
+  (nth-sig-fig 2 x))
+
+
+(fact "default frequency is A"
+      (frequency) => 440.0)
+
+(fact "frequency offset one up is A#"
+      (two-sig-fig (frequency 1)) => 466.16)
+
+(fact "the first value in the lazy freq stream is A"
+      (nth-sig-fig 5 (nth (freq-freq-lazy-seq 440.0) 0)) => 440.0)
+
+(fact "the 48001th value in the lazy freq stream is A#"
+      (nth-sig-fig 8 (nth (freq-freq-lazy-seq 440.0) 48000)) => (nth-sig-fig 8 (frequency 1)))
+
+(fact "frequency offset one up and then one down is A"
+      (frequency -1 (frequency 1)) => 440.0)
+
+(fact "frequency offset one down is G#"
+      (two-sig-fig (frequency -1)) => 415.30)
+
+(fact "frequency offset twelve down is A below middle A"
+      (two-sig-fig (frequency -12)) => 220.000)
+
+(fact "frequency offset twelve up is A above middle A"
+      (two-sig-fig (frequency 12)) => 880.000)
+
+(fact "get note gives 48000 frames of data"
+      (count  (get-note 0) ) => 48000)
+
+(fact "get note for 500 ms gives 24000 frames of data"
+      (count  (get-note 0 500) ) => 24000)
+
+(fact "get note for 100 ms gives 4800 frames of data"
+      (count  (get-note 0 100) ) => 4800)
+
