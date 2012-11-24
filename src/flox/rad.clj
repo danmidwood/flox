763d8fae0f8ba5d7048aff18ce28cfec82a23681
Dan Midwood <dan@danmidwood.com>
2012-11-24 12:43:07 +0000

    Add looping to the frequency generator. Transform the frequencies into sine values, and add a byte-my-sine fn to transform those values into bytes ready to be written to the line

--- src/flox/rad.clj
+++ src/flox/rad.clj
@@ -9,6 +9,7 @@
 (def line-data
   {:sample-rate 48000
    :audio-format (AudioFormat. 48000 16 2 true false)
+   :buffer-size (/ 48000 10)
    :12th-root (clojure.math.numeric-tower/expt 2 (/ 1 12))})
 
 
@@ -24,11 +25,45 @@
 
 (defn freq-freq-lazy-seq 
   "Create a lazy sequence of incrementing frequencies at one note per second with the specified sample-rate. If no sample-rate is supplied then 48000 is chosen"
-  ([base] (freq-freq-lazy-seq base 48000))
-  ([base sample-rate]
+  ([base] (freq-freq-lazy-seq base base (frequency 12 base)))
+  ([base min max]
      (lazy-seq
       (cons base
-            (freq-freq-lazy-seq (frequency (/ 1 sample-rate) base) sample-rate)))))
+            (if (> base max)
+              (freq-freq-lazy-seq (frequency (/ 1 (:sample-rate line-data)) min) min max)
+              (freq-freq-lazy-seq (frequency (/ 1 (:sample-rate line-data)) base) min max))))))
+
+
+(defn sine-seq
+  "Lazy seq for sine wave values"
+  ([freqs] (sine-seq freqs 0))
+  ([freqs angle]
+     (let [increment (* 2 (. Math PI) (/ (first freqs) (:sample-rate line-data)))
+           this-sine (clojure.algo.generic.math-functions/sin angle)]
+       (lazy-seq
+        (cons this-sine
+              (sine-seq (rest freqs) (+ angle increment)))))))
+
+(defn byte-my-sine
+  "Transforms a floating point sine value into a byte"
+  ([sine]
+     (byte (* (. Byte MAX_VALUE) sine))))
+
+(defn create-line
+  []
+  (let [line (. AudioSystem getSourceDataLine (:audio-format line-data))]
+    (doto line
+      (.open (:audio-format line-data) (:sample-rate line-data))
+      (.start))))
+
+(defn emit-audio
+  [line data]
+    (+
+     (.write ^SourceDataLine line (byte-array (take (:buffer-size line-data) data)) 0 (:buffer-size line-data))
+     (emit-audio line (drop (:buffer-size line-data) data))))
+
+
+
 
 
 
