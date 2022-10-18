ac0f0e3005aa0191e3f1416aa64ee6c6af550589
Yan <yan@metatem.net>
2022-10-18 03:50:39 +0200

    Upgrade deps

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/algo.generic "0.1.0"]
-                 [org.clojure/math.numeric-tower "0.0.1"]
-                 [midje "1.5-alpha2"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/algo.generic "0.1.3"]
+                 [org.clojure/math.numeric-tower "0.0.5"]
+                 [midje "1.10.6"]]
   :profiles {:dev {:plugins [[lein-midje "2.0.1"]]}}
   :main flox.main)
