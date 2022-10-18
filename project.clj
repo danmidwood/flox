7f891a65e9f20515e86bf279cec3e88611a53087
Yan <yan@metatem.net>
2022-10-18 03:58:31 +0200

    Update description and URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject flox "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Shepard Tone Generator"
+  :url "https://github.com/danmidwood/flox"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.1"]
